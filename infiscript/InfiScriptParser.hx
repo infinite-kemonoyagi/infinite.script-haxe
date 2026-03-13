a2ee43a92311dc952c23bb84868f62b8e347c754
infinite-kemonoyagi <infidoggy.newgrounds@gmail.com>
2026-03-13 11:00:04 -0700

    clean tab spaces in scripted code

--- infiscript/InfiScriptParser.hx
+++ infiscript/InfiScriptParser.hx
@@ -1,9 +1,17 @@
 package infiscript;
 
+using StringTools;
+
+@:nullSafety
 class InfiScriptParser
 {
-    public function new()
+    private final spaceReg:EReg = ~/\s+/g;
+
+    public function new() {}
+
+    public function runCode(code:String)
     {
-        
+        final codeCleanned:String = spaceReg.replace(code, " ");
+        trace(codeCleanned);
     }
 }
