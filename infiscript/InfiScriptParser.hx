833720641dace02853b20e5310ed3a0f6de83eb7
infinite-kemonoyagi <infidoggy.newgrounds@gmail.com>
2026-05-07 07:56:30 -0700

    restructuring all the code

--- infiscript/InfiScriptParser.hx
+++ infiscript/InfiScriptParser.hx
@@ -1,17 +1,4 @@
 package infiscript;
 
-using StringTools;
-
 @:nullSafety
-class InfiScriptParser
-{
-    private final spaceReg:EReg = ~/\s+/g;
-
-    public function new() {}
-
-    public function runCode(code:String)
-    {
-        final codeCleanned:String = spaceReg.replace(code, " ");
-        trace(codeCleanned);
-    }
-}
+class InfiScriptParser {}
--- test/source/Main.hx
+++ test/source/Main.hx
@@ -5,47 +5,5 @@ import infiscript.InfiScriptParser;
 @:nullSafety
 class Main
 {
-    @:nullSafety(Off)
-    private static var parser:InfiScriptParser;
-
-    public static function main():Void
-    {
-        parser = new InfiScriptParser();
-
-        simpleTrace();
-        usingVariables();
-        usingFunctions();
-    }
-
-    private static function simpleTrace():Void
-    {
-        parser.runCode('trace(\"Hello, World!\");');
-        parser.runCode('trace(\"I  HAVE  TOO  MANY  SPACES\");');
-    }
-
-    private static function usingVariables():Void
-    {
-        parser.runCode('variable name String = \"Infinite Kemonoyagi\";
-        variable age Int = 16;
-
-        trace(name);
-        trace(age);
-        ');
-    }
-
-    private static function usingFunctions():Void
-    {
-        parser.runCode('setup();
-
-        function setup() Void
-        {
-            trace(countApples(2));
-        }
-
-        function countApples(num Int) String
-        {
-            return \'There are $$num apples\';
-        }
-        ');
-    }
+    public static function main():Void {}
 }
